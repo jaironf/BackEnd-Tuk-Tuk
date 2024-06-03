98c2124a85b05d1bbb5621d05498b30a1770c9bb
jaironf <jaironf.94@gmail.com>
2024-06-03 12:42:54 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { dbConnection } = require("./config/config")
+const app = express()
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+
+dbConnection()
+app.use(express.json())
+app.use('/users', require('./routes/users'));
+app.use("/posts", require("./routes/posts"));
+app.use("/comments", require("./routes/comments"));
+
+app.listen(PORT, ()=> console.log(`Servidor levantado en el puerto ${PORT}`))
+
+module.exports = app;
