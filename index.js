a21718b93626158a3a02366ea349f8af330efc6f
jaironf <jaironf.94@gmail.com>
2024-06-04 15:13:23 +0200

    Cors implementado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { dbConnection } = require("./config/config")
 const app = express()
 require("dotenv").config();
+
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
